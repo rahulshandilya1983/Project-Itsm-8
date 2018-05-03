394de9ba1eef5632fa252a9eb296b938148fbca2
rahul <rahul@192.168.0.11>
2018-05-02 21:23:11 -0400

    d

--- __init__.py
+++ __init__.py
@@ -67,7 +67,7 @@ class ItsmUserSkill(MycroftSkill):
         length = len(data['result'])
         x = 0
         r = data['result']
-        number = r[x]['number']
+        number = r['result']['number']
         #detail = ""
         #for x in range(0, length):
             #detail += "Your Incident {}".format(x+1) +" is "+ r[x]['number'] + " having caller as "+r[x]['caller_id']+ " with Short Description "+ r[x]['short_description'] + " and priority as "+r[x]['priority']
