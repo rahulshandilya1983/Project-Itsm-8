262871a4a7f09a12a3eea82c4faa9b507efbd9f1
rahul <rahul@192.168.0.11>
2018-05-02 21:14:22 -0400

    d

--- __init__.py
+++ __init__.py
@@ -71,10 +71,11 @@ class ItsmUserSkill(MycroftSkill):
         #detail = ""
         #for x in range(0, length):
             #detail += "Your Incident {}".format(x+1) +" is "+ r[x]['number'] + " having caller as "+r[x]['caller_id']+ " with Short Description "+ r[x]['short_description'] + " and priority as "+r[x]['priority']
-        detail = "Your incident {} is {} having caller as {} with Short Description {} and priority as {}".format(x+1,  r[x]['number'], r[x]['caller_id'],r[x]['short_description'], r[x]['priority'])
+        #detail = "Your incident {} is {} having caller as {} with Short Description {} and priority as {}".format(x+1,  r[x]['number'], r[x]['caller_id'],r[x]['short_description'], r[x]['priority'])
         self.speak("Your Incident is {}".format(r))
-        self.log.debug("Detail: "+str(detail))
-        self.log.info('Details'+str(detail))
+        self.log.debug_print(r)
+        #self.log.debug("Detail: "+str(detail))
+        #self.log.info('Details'+str(detail))
             # + ' having caller as '+r[x]['caller_id']+ ' with Short Description '+ r[x]['short_description'] + ' and priority as '+r[x]['priority'])
             #x += 1
         
